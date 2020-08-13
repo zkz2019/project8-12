9aa0de46358f1a4f15e6830d9ac8fc52be2c39be
qiqi <1287804548@qq.com>
2020-08-13 11:33:49 +0800

    第二次测试提交

--- server/app/router.js
+++ server/app/router.js
@@ -3,6 +3,7 @@
 /**
  * @param {Egg.Application} app - egg application
  */
+console.log('aaa');
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
